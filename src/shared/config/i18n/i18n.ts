ddf441ac4498d84b56915704fdd3aa7da5e9d9a3
Robert Goncharenko <goncharenkorobert@gmail.com>
2023-11-02 22:04:46 +0100

    add eslint, fix files

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -1,8 +1,8 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
 
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 i18n
   .use(Backend)
@@ -10,16 +10,15 @@ i18n
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
-    debug: __IS_DEV__, //process.env.NODE_ENV === 'development'
+    debug: __IS_DEV__,
 
     interpolation: {
-      escapeValue: false,
+      escapeValue: false
     },
     backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
+      loadPath: '/locales/{{lng}}/{{ns}}.json'
     }
 
-  });
+  })
 
-
-export default i18n;
+export default i18n
