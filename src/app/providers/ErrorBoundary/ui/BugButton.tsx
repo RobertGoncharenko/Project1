ecadf7b20953a2b992246462acba9d2df64caf5d
Robert Goncharenko <goncharenkorobert@gmail.com>
2023-11-06 15:46:30 +0100

    add page loader, add notFound page, add error boundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,24 @@
+import { Button } from 'shared/ui/Button/Button';
+import { useEffect, useState } from 'react';
+
+interface BugButtonProps {
+  className?: string;
+}
+// Компонент для тестирования
+export const BugButton = ({ className }: BugButtonProps) => {
+    const [error, setError] = useState(false);
+    const onThrow = () => setError(true);
+    useEffect(() => {
+        if (error) {
+            throw new Error();
+        }
+    }, [error]);
+    return (
+        <Button
+            onClick={onThrow}
+        // eslint-disable-next-line i18next/no-literal-string
+        >
+            throw error
+        </Button>
+    );
+};
